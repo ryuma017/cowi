e5c481c05562a9efd7bb857f7821345917bf070a
ryuma017 <tgcrym01@gmail.com>
2022-07-13 01:21:13 +0900

    impl lexer

---
+++ src/instruction.rs
@@ -0,0 +1,55 @@
+/// The commands of COW.
+///
+/// | Code | Instruction | Description |
+/// |------|-------------|-------------|
+/// | 0 | moo | This command is connected to the MOO command. When encountered during normal execution, it searches the program code in reverse looking for a matching MOO command and begins executing again starting from the found MOO command. When searching, it skips the instruction that is immediately before it (see MOO).
+/// | 1 | mOo | Moves current memory position back one block.
+/// | 2 | moO | Moves current memory position forward one block.
+/// | 3 | mOO | Execute value in current memory block as if it were an instruction. The command executed is based on the instruction code value (for example, if the current memory block contains a 2, then the moO command is executed). An invalid command exits the running program. Value 3 is invalid as it would cause an infinite loop.
+/// | 4 | Moo | If current memory block has a 0 in it, read a single ASCII character from STDIN and store it in the current memory block. If the current memory block is not 0, then print the ASCII character that corresponds to the value in the current memory block to STDOUT.
+/// | 5 | MOo | Decrement current memory block value by 1.
+/// | 6 | MoO | Increment current memory block value by 1.
+/// | 7 | MOO | If current memory block value is 0, skip next command and resume execution **after** the next matching moo command. If current memory block value is not 0, then continue with next command. **Note that the fact that it skips the command immediately following it has interesting ramifications for where the matching moo command really is. For example, the following will match the second and not the first moo: OOO MOO moo moo**
+/// | 8 | OOO | Set current memory block value to 0.
+/// | 9 | MMM | If no current value in register, copy current memory block value. If there is a value in the register, then paste that value into the current memory block and clear the register.
+/// | 10 | OOM | Print value of current memory block to STDOUT as an integer.
+/// | 11 | oom | Read an integer from STDIN and put it into the current memory block.
+#[derive(Debug)]
+pub enum Instruction {
+    LoopEnd,          // moo
+    DecrementPointer, // mOo
+    IncrementPointer, // moO
+    ExecuteValue,     // mOO
+    ReadOrWrite,      // Moo
+    DecrementByte,    // MOo
+    IncrementByte,    // MoO
+    LoopBigin,        // MOO
+    ReadStdin,        // oom
+    SetZero,          // OOO
+    CopyOrPaste,      // MMM
+    WriteStdout,      // OOM
+}
+
+pub trait AsInstruction {
+    fn as_instruction(&self) -> Option<Instruction>;
+}
+
+impl AsInstruction for [u8] {
+    fn as_instruction(&self) -> Option<Instruction> {
+        match self {
+            b"moo" => Some(Instruction::LoopEnd),
+            b"mOo" => Some(Instruction::DecrementPointer),
+            b"moO" => Some(Instruction::IncrementPointer),
+            b"mOO" => Some(Instruction::ExecuteValue),
+            b"Moo" => Some(Instruction::ReadOrWrite),
+            b"MOo" => Some(Instruction::DecrementByte),
+            b"MoO" => Some(Instruction::IncrementByte),
+            b"MOO" => Some(Instruction::LoopBigin),
+            b"oom" => Some(Instruction::ReadStdin),
+            b"OOO" => Some(Instruction::SetZero),
+            b"MMM" => Some(Instruction::CopyOrPaste),
+            b"OOM" => Some(Instruction::WriteStdout),
+            _ => None,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::{path::PathBuf, fs::File, io::{Read, Cursor}};
+
+use anyhow::Context;
+use clap::Parser;
+
+use cowi::instruction::AsInstruction;
+
+#[derive(Parser)]
+#[clap(about, version, author, long_about = None)]
+struct Arg {
+    /// Path to COW file
+    #[clap(parse(from_os_str))]
+    file_path: PathBuf
+}
+
+fn main() -> anyhow::Result<()> {
+    let arg = Arg::parse();
+    let bytes: Vec<u8> = {
+        let mut bytes = vec![];
+        let mut file = File::open(&arg.file_path)
+            .with_context(|| format!("Failed to open `{}`", arg.file_path.display()))?;
+        file.read_to_end(&mut bytes)
+            .context("Failed to read bytes from source.")?;
+        bytes
+    };
+    let mut buffer = [0; 3];
+    let length = bytes.len();
+    let mut cursor = Cursor::new(bytes);
+    let mut program = vec![];
+    loop {
+        if cursor.position() == (length - 2) as u64 {
+            break;
+        }
+        match cursor.read_exact(&mut buffer) {
+            Ok(()) => {},
+            Err(e) => panic!("{e}")
+        };
+        if let Some(i) = buffer.as_instruction() {
+            program.push(i);
+        }
+        let current_pos = cursor.position();
+        cursor.set_position(current_pos - 2);
+    };
+    dbg!(program);
+    Ok(())
 }
